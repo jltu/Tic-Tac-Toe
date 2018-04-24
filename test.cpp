98298fbebe4a140153e07e4e664efcd51db03d4a
Andy Chu <andy@dhcp-168-105-240-63.wireless.manoa.hawaii.edu>
2018-04-23 16:52:15 -1000

    testing

---
+++ test.cpp
@@ -0,0 +1,8 @@
+//
+//  test.cpp
+//  
+//
+//  Created by Andy Chu on 4/23/18.
+//
+
+#include "test.hpp"
---
+++ test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  
+//
+//  Created by Andy Chu on 4/23/18.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
