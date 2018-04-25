6632bc2134e60cef38a03ed67180503e064328ab
dliang947 <davidliang947@gmail.com>
2018-04-24 18:47:04 -1000

    cleaned code, all my changes are on one file... messy

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <DEFINITIONS.hpp>
 //using namespace std;
 //using namespace sf;
 
+// Definitions can be moved to DEFINITIONS.hpp later on
 #define FPS 60.0f
 
 #define SCREEN_WIDTH  600
 #define SCREEN_HEIGHT 600
+
+#define BOARD_SCALE .33
 
 #define BOARD_WIDTH 467*BOARD_SCALE
 #define BOARD_LENGTH 466*BOARD_SCALE
@@ -15,39 +19,37 @@
 
 #define SPACE 60
 
+
 int main()
 {
   	// Create window // the style makes it so you can't resize the window
   	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), " \"3D\" Tic-Tac-Toe",sf::Style::Close);
 
-
-
-
-
-    // The boards
+    // Grid Texture
     sf::Texture grid;
   	if (!grid.loadFromFile("res/images/Grid.png"))
   		std::cout << "ERROR: Could not load Board" << std::endl;
 
-  	sf::Sprite b1;				// Centered Board
+    // Top-Left Board
+  	sf::Sprite b1;                            // Declares Sprite
   	b1.setScale(BOARD_SCALE,BOARD_SCALE);			// Scales width and height
-  	b1.setTexture(grid);				// assigns texture to the sprite
-  	b1.setPosition(SPACE, SPACE);	// positions sprite
+  	b1.setTexture(grid);				              // Assigns texture to the sprite
+  	b1.setPosition(SPACE, SPACE);	            // Positions sprite
 
-    sf::Sprite b2;				// Declares board
-  	b2.setScale(BOARD_SCALE,BOARD_SCALE);			// Scales width and height
-  	b2.setTexture(grid);				// assigns texture to the sprite
+    // Centered Board
+    sf::Sprite b2;
+  	b2.setScale(BOARD_SCALE,BOARD_SCALE);
+  	b2.setTexture(grid);
   	b2.setPosition(SCREEN_WIDTH/2 - (BOARD_WIDTH)/2, SCREEN_HEIGHT/2 - (BOARD_LENGTH)/2);	// centers sprite
 
-    sf::Sprite b3;				// Declares board
-    b3.setScale(BOARD_SCALE,BOARD_SCALE);			// Scales width and height
-    b3.setTexture(grid);				// assigns texture to the sprite
-    b3.setPosition(SCREEN_WIDTH - BOARD_WIDTH - SPACE, SCREEN_HEIGHT - BOARD_LENGTH - SPACE);	// positions sprite
+    // Bottom-Right Board
+    sf::Sprite b3;
+    b3.setScale(BOARD_SCALE,BOARD_SCALE);
+    b3.setTexture(grid);
+    b3.setPosition(SCREEN_WIDTH - BOARD_WIDTH - SPACE, SCREEN_HEIGHT - BOARD_LENGTH - SPACE);
 
 
-
-
-    // Game Loop
+//////////////////~~~~~~~~~ Game Loop  ~~~~~~~~~//////////////////
     while (window.isOpen())
     {
       // Event Loop    [Check each event with every iteration of loop]
@@ -56,11 +58,10 @@ int main()
       {
         switch (event.type)
         {
-          // To close the window
+          // Window can be closed by pressing Esc key or EOF
           case sf::Event::Closed:
             window.close();
             break;
-          // Shortcut to close window
           case sf::Event::KeyPressed:
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
             {
@@ -78,12 +79,21 @@ int main()
       window.clear(sf::Color(187,225,254));		// redraws background
 
 
-      // Draw everything here>>>
-      //window.draw(menu);
+// <<<<<<<<<<<<< Draw everything here >>>>>>>>>>>>>>>
+      // Draw the menu
+      // window.draw(menu);
+
+      // Draw the Grids
       window.draw(b1);
       window.draw(b2);
       window.draw(b3);
-      window.display();		// redraws the display
+
+      // Draw the Pieces
+
+
+
+      // Redraws the Display
+      window.display();
 
 
 
