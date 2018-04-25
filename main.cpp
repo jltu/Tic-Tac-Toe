ba068f9f0684b58ee4cf276a0fd61684ab5aa1a3
dliang947 <davidliang947@gmail.com>
2018-04-24 17:13:46 -1000

    added board to screen

--- main.cpp
+++ main.cpp
@@ -4,13 +4,27 @@
 //using namespace sf;
 
 #define FPS 60.0f
-#define SCREEN_WIDTH  680
-#define SCREEN_HEIGHT 680
+
+#define SCREEN_WIDTH  600
+#define SCREEN_HEIGHT 600
+
+#define BOARD_WIDTH 467
+#define BOARD_LENGTH 466
+
 
 int main()
 {
   	// Create window // the style makes it so you can't resize the window
   	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), " \"3D\" Tic-Tac-Toe",sf::Style::Close);
+
+    sf::Texture grid;
+  	if (!bTexture.loadFromFile("res/images/Grid.png"))
+  		std::cout << "ERROR: Could not load Board" << std::endl;
+
+  	sf::Sprite board;				// Declares board
+  	board.setScale(1,1);			// Scales width and height
+  	board.setTexture(grid);				// assigns texture to the sprite
+  	board.setPosition(SCREEN_WIDTH/2 - BOARD_WIDTH/2, SCREEN_HEIGHT/2 - BOARD_LENGTH/2);	// centers sprite
 
     // Game Loop
     while (window.isOpen())
@@ -43,6 +57,10 @@ int main()
       window.clear(sf::Color(187,225,254));		// redraws background
 
       // Draw everything here>>>
+      window.draw(board);
       window.display();		// redraws the display
+
+
+
     }
 }
