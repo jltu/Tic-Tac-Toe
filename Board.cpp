4ae40032cbc86b3137182701ae6a6cc77221f8e3
Joseph Chan <josephc2@hawaii.edu>
2018-04-29 21:28:53 -1000

    added board and definitions

---
+++ Board.cpp
@@ -0,0 +1,21 @@
+#include "Board.hpp"
+
+Board::Board(float posX, float posY)
+{
+    grid.setSmooth(true);
+  	grid.loadFromFile("res/images/Grid.png");
+
+    board.setScale(BSCALE,BSCALE);
+  	board.setTexture(grid);
+  	board.setPosition(sf::Vector2f(posX,posY));
+}
+
+void Board::draw(sf::RenderWindow &window)
+{
+    window.draw(board);
+}
+
+sf::Sprite Board::getBoard()
+{
+    return Board;
+}
---
+++ Definitions.hpp
@@ -0,0 +1,13 @@
+#define FPS 60.0f
+
+#define SCREEN_WIDTH  600
+#define SCREEN_HEIGHT 600
+
+#define BSCALE .33
+
+#define BSIZE 466*BSCALE
+#define BSIZE 466*BSCALE
+
+#define SPACE 60
+
+#define PSIZE 160*BSCALE
